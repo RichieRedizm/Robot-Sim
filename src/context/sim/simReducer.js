f08a6be501c3f236f252537c5f67c81c85836494
Richie Redizm <richieredizm@gmail.com>
2020-05-04 11:48:38 +0100

    remove loading state and include check if robot is placed on table

--- src/context/sim/simReducer.js
+++ src/context/sim/simReducer.js
@@ -1,25 +1,13 @@
-import { MOVE_ROBOT, PLACE_ROBOT, SET_LOADING } from '../types'
+import { MOVE_ROBOT } from '../types'
 
 export default (state, action) => {
   switch (action.type) {
-    case PLACE_ROBOT:
-      return {
-        ...state,
-        position: action.payload,
-        loading: false,
-      }
     case MOVE_ROBOT:
       return {
         ...state,
         position: action.payload.position,
         robotClass: action.payload.robotClass,
         facing: action.payload.facing,
-        loading: false,
-      }
-    case SET_LOADING:
-      return {
-        ...state,
-        loading: true,
       }
     default:
       return state
